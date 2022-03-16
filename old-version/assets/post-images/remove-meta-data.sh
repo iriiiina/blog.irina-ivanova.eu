2805e576ca013964ee492c26a11d6d3782111bef
iriiiina <irina.ivanova@protonmail.com>
2022-03-16 22:49:17 +0200

    remove old-version directory

--- old-version/assets/post-images/remove-meta-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dir=$1;
-
-find $dir -type f -name "*" |\
-  while read FILENAME
-  do
-    exiftool -all= -overwrite_original_in_place "${FILENAME}";
-  done
--- old-version/assets/post-images/resize-images.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dir=$1;
-
-sips -Z 1000 $dir/*;
-sips -Z 400 $dir/preview.*;
